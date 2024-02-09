2cb2df6107c2934272dac8d56709470470eb6ae5
Sergey Burish <>
2024-02-09 18:13:11 +0300

    pagination fix: state update

--- lib/presentation/components/pagination_control.dart
+++ lib/presentation/components/pagination_control.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:test_player/internal/dependencies/state_module.dart';
 import 'package:test_player/l10n/l10n.dart';
 
@@ -9,24 +10,26 @@ class PaginationControl extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        if (appState.currentVideosPage > 1)
-          IconButton(
-            icon: const Icon(Icons.arrow_back_ios),
-            tooltip: L10n.of(context).previous,
-            onPressed: () => appState.getVideosPrevPage(),
-          ),
-        Text('${appState.currentVideosPage}'),
-        if (appState.hasMore)
-          IconButton(
-            icon: const Icon(Icons.arrow_forward_ios),
-            tooltip: L10n.of(context).next,
-            onPressed: () => appState.getVideosNextPage(),
-          ),
-      ],
-    );
+    return Observer( builder: (_) {
+      return Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          if (appState.currentVideosPage > 1)
+            IconButton(
+              icon: const Icon(Icons.arrow_back_ios),
+              tooltip: L10n.of(context).previous,
+              onPressed: () => appState.getVideosPrevPage(),
+            ),
+          Text('${appState.currentVideosPage}'),
+          if (appState.hasMore)
+            IconButton(
+              icon: const Icon(Icons.arrow_forward_ios),
+              tooltip: L10n.of(context).next,
+              onPressed: () => appState.getVideosNextPage(),
+            ),
+        ],
+      );
+    });
   }
 }
