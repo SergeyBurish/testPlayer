0ffbcba399a22feb7c6b82e4d9a77d7465beeccc
Sergey Burish <>
2024-02-09 19:52:51 +0300

    images in assets to avoid CORS policy restriction

--- lib/data/mocks/data_provider_mocks.dart
+++ lib/data/mocks/data_provider_mocks.dart
@@ -72,7 +72,7 @@ final class VideosDataProviderMock implements VideosDataProvider {
 final Map<String, dynamic> _main = {
   'id': 10,
   'image':
-      'https://res.cloudinary.com/momentum-media-group-pty-ltd/image/upload/v1686795211/Space%20Connect/space-exploration-sc_fm1ysf.jpg',
+      'assets/id10-mock.jpg',
   'name': 'NASA plans to open the International Space Station for business',
   'url': '-', // video url
   'duration': 3620,
@@ -85,7 +85,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 25,
     'image':
-        'https://i0.wp.com/www.sciencenews.org/wp-content/uploads/2022/11/Hubble-Pillars-of-Creation.jpg?resize=1500%2C1565&ssl=1',
+        'assets/id25-mock.webp',
     'name': 'These are our top space images of all time',
     'url': '-', // video url
     'duration': 4210,
@@ -96,7 +96,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 24,
     'image':
-        'https://www.krqe.com/wp-content/uploads/sites/12/2022/12/AdobeStock_81556974.jpeg?w=2560&h=1440&crop=1',
+        'assets/id24-mock.webp',
     'name': 'Space discoveries that will blow your mind',
     'url': '-', // video url
     'duration': 5312,
@@ -107,7 +107,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 23,
     'image':
-        'https://images.hindustantimes.com/tech/img/2023/02/15/960x540/asteroid_1676434888953_1676434897704_1676434897704.jpg',
+        'assets/id23-mock.jpg',
     'name': 'Asteroid today: Space rock speeding towards Earth at a fearsome 60905 kmph',
     'url': '-', // video url
     'duration': 245,
@@ -118,7 +118,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 22,
     'image':
-        'https://i0.wp.com/spacenews.com/wp-content/uploads/2023/11/Millennium_Space_Systems_completes_MTC-scaled.jpg?fit=1200%2C750&ssl=1',
+        'assets/id22-mock.webp',
     'name': '20 amazing facts about space and astronomy',
     'url': '-', // video url
     'duration': 6541,
@@ -129,7 +129,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 21,
     'image':
-        'https://img.freepik.com/free-photo/glowing-spaceship-orbits-planet-starry-galaxy-generated-by-ai_188544-9655.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705795200&semt=sph',
+        'assets/id21-mock.jpg',
     'name':
         'Space Force\'s first six missile-defense satellites in medium orbit to cost about \$500 million',
     'url': '-', // video url
@@ -140,7 +140,7 @@ final List<Map<String, dynamic>> _all = [
   },
   {
     'id': 20,
-    'image': 'https://www.witf.io/wp-content/uploads/2020/11/iStock-1159238834-1628x1080.jpg',
+    'image': 'assets/id20-mock.jpg',
     'name': 'Scientists discover outer space isn\'t pitch-black after all',
     'url': '-', // video url
     'duration': 7634,
@@ -151,7 +151,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 19,
     'image':
-        'https://www.polytechnique-insights.com/wp-content/uploads/2022/11/adobestock_402438380-1-1537x600.jpeg',
+        'assets/id19-mock.jpeg',
     'name': 'The changing face of the space industry',
     'url': '-', // video url
     'duration': 123,
@@ -163,7 +163,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 18,
     'image':
-        'https://i0.wp.com/www.sciencenews.org/wp-content/uploads/2022/11/Hubble-Pillars-of-Creation.jpg?resize=1500%2C1565&ssl=1',
+        'assets/id25-mock.webp',
     'name': 'AAA These are our top space images of all time',
     'url': '-', // video url
     'duration': 4210,
@@ -174,7 +174,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 17,
     'image':
-        'https://www.krqe.com/wp-content/uploads/sites/12/2022/12/AdobeStock_81556974.jpeg?w=2560&h=1440&crop=1',
+        'assets/id24-mock.webp',
     'name': 'AAA Space discoveries that will blow your mind',
     'url': '-', // video url
     'duration': 5312,
@@ -185,7 +185,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 16,
     'image':
-        'https://images.hindustantimes.com/tech/img/2023/02/15/960x540/asteroid_1676434888953_1676434897704_1676434897704.jpg',
+        'assets/id23-mock.jpg',
     'name': 'AAA Asteroid today: Space rock speeding towards Earth at a fearsome 60905 kmph',
     'url': '-', // video url
     'duration': 245,
@@ -196,7 +196,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 14,
     'image':
-        'https://i0.wp.com/spacenews.com/wp-content/uploads/2023/11/Millennium_Space_Systems_completes_MTC-scaled.jpg?fit=1200%2C750&ssl=1',
+        'assets/id22-mock.webp',
     'name': 'BBB 20 amazing facts about space and astronomy',
     'url': '-', // video url
     'duration': 6541,
@@ -207,7 +207,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 13,
     'image':
-        'https://img.freepik.com/free-photo/glowing-spaceship-orbits-planet-starry-galaxy-generated-by-ai_188544-9655.jpg?size=626&ext=jpg&ga=GA1.1.632798143.1705795200&semt=sph',
+        'assets/id21-mock.jpg',
     'name':
         'BBB Space Force\'s first six missile-defense satellites in medium orbit to cost about \$500 million',
     'url': '-', // video url
@@ -218,7 +218,7 @@ final List<Map<String, dynamic>> _all = [
   },
   {
     'id': 12,
-    'image': 'https://www.witf.io/wp-content/uploads/2020/11/iStock-1159238834-1628x1080.jpg',
+    'image': 'assets/id20-mock.jpg',
     'name': 'BBB Scientists discover outer space isn\'t pitch-black after all',
     'url': '-', // video url
     'duration': 7634,
@@ -229,7 +229,7 @@ final List<Map<String, dynamic>> _all = [
   {
     'id': 11,
     'image':
-        'https://www.polytechnique-insights.com/wp-content/uploads/2022/11/adobestock_402438380-1-1537x600.jpeg',
+        'assets/id19-mock.jpeg',
     'name': 'BBB The changing face of the space industry',
     'url': '-', // video url
     'duration': 123,
